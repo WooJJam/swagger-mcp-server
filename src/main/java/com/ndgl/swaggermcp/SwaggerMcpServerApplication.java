bb91b6c91a733829f26ad9d9c54e75952e8456d8
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-01-30 18:12:16 +0900

    init: 초기 프로젝트 생성

---
+++ src/main/java/com/ndgl/swaggermcp/SwaggerMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.ndgl.swaggermcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerMcpServerApplication.class, args);
+    }
+
+}
